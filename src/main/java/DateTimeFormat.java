643cd0030af9cdc3e090dfe8c497fc2cadcc87a0
Mingyuan03 <e1115303@u.nus.edu>
2025-02-03 05:18:48 +0800

    Modify task times to understand only ISO8601-like LocalDateTime formats and print to console likewise to minimise ambiguity

---
+++ src/main/java/DateTimeFormat.java
@@ -0,0 +1,52 @@
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.util.Optional;
+
+public enum DateTimeFormat {
+    // Datetime formats below
+    ISO8601("yyyy-MM-dd'T'HH:mm"), // hyphenated date
+    SPACING("yyyy-MM-dd HH:mm"), // spacing logically implies format has time
+    DATETIME_SLASH("yyyy/MM/dd HH:mm"),
+    DATETIME_TEXT("dd MMMM yyyy HH:mm"),
+    DATETIME_TEXT_ABBREV("dd MMM yyyy HH:mm"),
+    DATETIME_WEEKDAY("EEEE, yyyy-MM-dd HH:mm"),
+    DATETIME_WEEKDAY_ABBREV("E, yyyy-MM-dd HH:mm"),
+    DATETIME_COMPACT("yyyyMMdd HH:mm"),
+    // Date-only formats below
+    ISO8601_DATE("yyyy-MM-dd"),
+    DATE_SLASH("yyyy/MM/dd"),
+    DATE_TEXT("dd MMMM yyyy"),
+    DATE_TEXT_ABBREV("dd MMM yyyy"),
+    DATE_WEEKDAY("EEEE, yyyy-MM-dd"),
+    DATE_WEEKDAY_ABBREV("E, yyyy-MM-dd"),
+    DATE_COMPACT("yyyyMMdd");
+
+    private final DateTimeFormatter formatter;
+
+    DateTimeFormat(String pattern) {
+        this.formatter = DateTimeFormatter.ofPattern(pattern);
+    }
+
+    public static Optional<LocalDateTime> parseDateTime(String timeString) {
+        for (DateTimeFormat format : new DateTimeFormat[] {ISO8601, SPACING, DATETIME_SLASH, DATETIME_TEXT,
+                DATETIME_TEXT_ABBREV, DATETIME_WEEKDAY, DATETIME_WEEKDAY_ABBREV, DATETIME_COMPACT}) {
+            try {
+                return Optional.of(LocalDateTime.parse(timeString, format.formatter));
+            } catch (DateTimeException ignored) {}
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<LocalDate> parseDate(String timeString) {
+        for (DateTimeFormat format : new DateTimeFormat[] {ISO8601_DATE, DATE_SLASH, DATE_TEXT, DATE_TEXT_ABBREV,
+                DATE_WEEKDAY, DATE_WEEKDAY_ABBREV, DATE_COMPACT}) {
+            try {
+                return Optional.of(LocalDate.parse(timeString, format.formatter));
+            } catch (DateTimeException ignored) {}
+        }
+        return Optional.empty();
+    }
+}
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,6 +2,8 @@ public abstract class Task {
     protected String description; // protected so that it's accessible in subclasses only.
     private boolean isDone; // protected so that it's accessible in subclasses only.
 
+    // private static final Pattern ISO8601PATTERN = Pattern.compile("^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])")
+
     public String getDescription() {
         return this.description;
     }
